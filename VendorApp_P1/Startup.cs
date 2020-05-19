d3f524af981f63b45871b4b98f39075aa9a92165
Mohcka <mikeduels@yahoo.com>
2020-05-18 20:03:07 -0700

    Removes Cors ❌

--- VendorApp_P1/Startup.cs
+++ VendorApp_P1/Startup.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Microsoft.AspNetCore.Cors; // TODO: remove later
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
